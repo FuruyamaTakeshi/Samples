f3005c02e7f5ca660f85f1d80dc68f5e5c7b4243
Furuyama Takeshi <Furuyama.Takeshi0121@gmail.com>
2017-08-09 00:25:51 +0900

    add Decolator Playgound

---
+++ GoF_DesignPattern/Playground/DecoratorPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+class PrintImage {
+    func renderImage() {
+        print("image")
+    }
+}
+
+class BodyImage: PrintImage {
+    override func renderImage() {
+        for idx in 0..<10 {
+            print("item: \(idx)")
+        }
+    }
+}
+
+class DecolateImage: PrintImage {
+    let image: PrintImage
+    init(image: PrintImage) {
+        self.image = image
+    }
+    
+    override func renderImage() {
+        print("###########")
+        image.renderImage()
+        print("###########")
+
+    }
+}
+
+class StoreInfoImage: DecolateImage {
+    override func renderImage() {
+        print("Store name")
+        print("Tel: xxx-xxx-xxxx")
+        image.renderImage()
+    }
+}
+
+class PaymentImage: DecolateImage {
+    override func renderImage() {
+        image.renderImage()
+        print("cash $12.34")
+    }
+}
+
+PaymentImage(image:
+    StoreInfoImage(image:
+        DecolateImage(image:
+            BodyImage()))).renderImage()
