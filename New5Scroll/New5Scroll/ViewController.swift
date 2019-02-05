d333b1f00b0f0ee18467a2d3c02a526d9fd07bb8
Furuyama.Takeshi <Furuyma.Takeshi0121@gmail.com>
2019-02-05 09:12:02 +0900

    add New5Scroll

---
+++ New5Scroll/New5Scroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New5Scroll
+//
+//  Created by Furuken on 2019/02/04.
+//  Copyright © 2019 古山健司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
