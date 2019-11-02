63c80eaf346a2a8b9329afd0da6e34f99153be98
Furuyama.Takeshi <Furuyma.Takeshi0121@gmail.com>
2019-11-02 19:22:18 +0900

    Revert "add kotlinx serialize"
    This reverts commit 6828f447285f287c1781833ba8d39edb290db283.

--- KotlinNativeiOS/sharedNative/src/commonMain/kotlin/common.kt
+++ KotlinNativeiOS/sharedNative/src/commonMain/kotlin/common.kt
@@ -1,23 +1,7 @@
 package org.kotlin.mpp.mobile
 
-import kotlinx.serialization.*
-import kotlinx.serialization.json.*
-
-@Serializable data class UserEntity(val id: String, val name: String)
-
-@Serializable data class Data(val a: Int, val b: String = "42")
-
 fun helloWorld() {
-    print("hello MPP world")
-    val json = Json(JsonConfiguration.Stable)
-    // serializing objects
-    val jsonData = json.stringify(Data.serializer(), Data(42))
-    // serializing lists
-    val jsonList = json.stringify(Data.serializer().list, listOf(Data(42)))
-    println(jsonData) // {"a": 42, "b": "42"}
-    println(jsonList) // [{"a": 42, "b": "42"}]
-
-    // parsing data back
-    val obj = json.parse(Data.serializer(), """{"a":42}""") // b is optional since it has default value
-    println(obj) // Data(a=42, b="42")
+    println("Hello World!")
 }
+
+data class UserEntity(val id: String, val name: String)
