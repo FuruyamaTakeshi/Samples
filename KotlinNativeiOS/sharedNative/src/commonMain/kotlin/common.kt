6828f447285f287c1781833ba8d39edb290db283
Furuyama.Takeshi <Furuyma.Takeshi0121@gmail.com>
2019-11-02 18:56:47 +0900

    add kotlinx serialize

--- KotlinNativeiOS/sharedNative/src/commonMain/kotlin/common.kt
+++ KotlinNativeiOS/sharedNative/src/commonMain/kotlin/common.kt
@@ -1,7 +1,23 @@
 package org.kotlin.mpp.mobile
 
-fun helloWorld() {
-    println("Hello World!")
-}
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
-data class UserEntity(val id: String, val name: String)
+@Serializable data class UserEntity(val id: String, val name: String)
+
+@Serializable data class Data(val a: Int, val b: String = "42")
+
+fun helloWorld() {
+    print("hello MPP world")
+    val json = Json(JsonConfiguration.Stable)
+    // serializing objects
+    val jsonData = json.stringify(Data.serializer(), Data(42))
+    // serializing lists
+    val jsonList = json.stringify(Data.serializer().list, listOf(Data(42)))
+    println(jsonData) // {"a": 42, "b": "42"}
+    println(jsonList) // [{"a": 42, "b": "42"}]
+
+    // parsing data back
+    val obj = json.parse(Data.serializer(), """{"a":42}""") // b is optional since it has default value
+    println(obj) // Data(a=42, b="42")
+}
