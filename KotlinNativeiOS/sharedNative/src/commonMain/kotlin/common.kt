05183b6c77590a2e216823743442d25f6009dea1
Furuyama.Takeshi <Furuyma.Takeshi0121@gmail.com>
2019-11-12 05:49:25 +0900

    add ktor-client

--- KotlinNativeiOS/sharedNative/src/commonMain/kotlin/common.kt
+++ KotlinNativeiOS/sharedNative/src/commonMain/kotlin/common.kt
@@ -2,6 +2,10 @@ package org.kotlin.mpp.mobile
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.*
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.coroutines.*
 
 fun helloWorld() {
     println("Hello World!")
@@ -19,4 +23,24 @@ fun helloSerializable() {
     val json = Json(JsonConfiguration.Stable)
     val jsonData = json.stringify(UserEntity.serializer(), UserEntity("1234", "taro", 30))
     println(jsonData)
+}
+
+internal expect val ApplicationDispatcher: CoroutineDispatcher
+
+class ApplicationApi {
+    private val client = HttpClient()
+
+    var address = Url("https://tools.ietf.org/rfc/rfc1866.txt")
+
+    fun about(callback: (String) -> Unit) {
+        GlobalScope.apply {
+            launch(ApplicationDispatcher) {
+                val result: String = client.get {
+                    url(this@ApplicationApi.address.toString())
+                }
+
+                callback(result)
+            }
+        }
+    }
 }
